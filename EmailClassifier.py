652690cafedc6e8c8038158cf30ae615479b31b3
Shunyu Wang <shunyu.wang@sap.com>
2019-03-30 16:01:26 -0400

    initialize the project

---
+++ EmailClassifier.py
@@ -0,0 +1,20 @@
+import os
+
+file_names = [] #training file names read from the give directory
+ham_count = 0 #number of ham email
+spam_count = 0 #number of spam email
+
+ham_words_count = 0 #total words count in ham 
+ham_word_count_dict = {} #each word with its count in ham
+spam_words_count = 0 #total words ounnt in spam
+spam_word_count_dict = {} #each word with its count in spam
+
+training_set_directory = "train/"
+
+
+def read_file_names_in_directory():
+    global file_names, training_set_directory
+    file_names = os.listdir(training_set_directory)
+
+def training_with_one_email(file_path):
+    return 1
