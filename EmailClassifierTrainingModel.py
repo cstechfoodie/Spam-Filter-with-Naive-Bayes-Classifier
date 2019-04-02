a284e2918a46ccc20986959bfd8e530a3d08fec9
Shunyu Wang <w_shunyu@live.concordia.ca>
2019-04-01 20:51:34 -0400

    clean code

--- EmailClassifierTrainingModel.py
+++ EmailClassifierTrainingModel.py
@@ -23,7 +23,6 @@ generated_model_file = "model.txt"
 baseline_result = "baseline-result.txt"  # Experiment 1
 stopword_result = "stopword-result.txt"  # Experiment 2
 wordlength_result = "wordlength_result.txt"  # Experiment 3
-
 '''
 Read all file names given the directory name, we need their names to identify whether it is a ham or spam
 Here is where we save all traing files
@@ -33,13 +32,14 @@ Here is where we save all traing files
 def read_file_names_in_directory(data_set_directory):
     return os.listdir(data_set_directory)
 
+
 # training with a single file. need to save all info to their persistences
 
 
-def training_with_one_email(file_path, tokens_count, token_count_dict, token_prob_dict):
+def training_with_one_email(file_path, tokens_count, token_count_dict,
+                            token_prob_dict):
     global all_tokens
-    f = open(training_set_directory + file_path,
-             "r", encoding="iso8859_2")
+    f = open(training_set_directory + file_path, "r", encoding="iso8859_2")
     lines = f.read().splitlines()
     for line in lines:
         token_list = re.split("[^a-zA-Z]", line)
@@ -47,7 +47,6 @@ def training_with_one_email(file_path, tokens_count, token_count_dict, token_pro
             if token.strip():
                 token = str(token).lower()
                 tokens_count[0] = tokens_count[0] + 1
-                print(tokens_count[0])
             else:
                 continue
             if token in token_count_dict:
@@ -56,6 +55,7 @@ def training_with_one_email(file_path, tokens_count, token_count_dict, token_pro
                 token_count_dict[token] = 1
             if token not in all_tokens:
                 all_tokens.append(token)
+
 
 # smooth with 0.5 - assume each token will at least show 0.5 time
 
@@ -67,13 +67,15 @@ def calculate_probabilities():
     for token in all_tokens:
         if token in ham_token_count_dict.keys():
             ham_token_prob_dict[token] = (
-                ham_token_count_dict[token] + smooth_constant) / (ham_tokens_count[0] + smooth_constant * vocabulary_len)
+                ham_token_count_dict[token] + smooth_constant) / (
+                    ham_tokens_count[0] + smooth_constant * vocabulary_len)
         else:
             ham_token_prob_dict[token] = smooth_constant / \
                 (ham_tokens_count[0] + smooth_constant * vocabulary_len)
         if token in spam_token_count_dict.keys():
             spam_token_prob_dict[token] = (
-                spam_token_count_dict[token] + smooth_constant) / (spam_tokens_count[0] + smooth_constant * vocabulary_len)
+                spam_token_count_dict[token] + smooth_constant) / (
+                    spam_tokens_count[0] + smooth_constant * vocabulary_len)
         else:
             spam_token_prob_dict[token] = smooth_constant / \
                 (spam_tokens_count[0] + smooth_constant * vocabulary_len)
@@ -96,7 +98,8 @@ def generate_model_file(file_name):
         toekn_prob_in_spam = float(spam_token_prob_dict[token])
 
         line = '%d  %s  %d %.7f  %d  %.7f\n' % (
-            line_counter, token, token_count_in_ham, toekn_prob_in_ham, token_count_in_spam, toekn_prob_in_spam)
+            line_counter, token, token_count_in_ham, toekn_prob_in_ham,
+            token_count_in_spam, toekn_prob_in_spam)
         f.write(line)
         line_counter = line_counter + 1
     f.close()
@@ -108,13 +111,13 @@ for file in file_names_for_training:
     if str(file).startswith("train-ham"):
         print("Currently Training with file: " + file)
         ham_count = ham_count + 1
-        training_with_one_email(file, ham_tokens_count,
-                                ham_token_count_dict, ham_token_prob_dict)
+        training_with_one_email(file, ham_tokens_count, ham_token_count_dict,
+                                ham_token_prob_dict)
     else:
         print("Currently Training with file: " + file)
         spam_count = spam_count + 1
-        training_with_one_email(file, spam_tokens_count,
-                                spam_token_count_dict, spam_token_prob_dict)
+        training_with_one_email(file, spam_tokens_count, spam_token_count_dict,
+                                spam_token_prob_dict)
 calculate_probabilities()
 generate_model_file(generated_model_file)
 
